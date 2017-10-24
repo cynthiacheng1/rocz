7a3b90b40cf08770aa5ac94f3e57b0a42b9b4e81
Jake Zaia <jzaia@stuy.edu>
2017-10-24 19:49:23 -0400

    Finished up part of the backend for homepage

--- utils/homepage.py
+++ utils/homepage.py
@@ -1,13 +1,22 @@
 import sqlite3
 
-db = sqlite3.connect('database.db')
-c = db.cursor()
-
-def getStories(n):
+## Returns a list of n tuples, each tuple being a story entry in the db
+def getStories(db, n):
+    c = db.cursor()
+    L = []
     for each in c.execute("SELECT * FROM stories"):
-        print each
-
+        L.append(each)
+    L = L[len(L)-n:]
+    return L[::-1]
 
 
 if __name__ == '__main__':
-    getStories(5)
+    db = sqlite3.connect('../database.db')
+    c=db.cursor()
+    for i in xrange(10):
+        c.execute("INSERT INTO stories VALUES (%d, \"%s\", \"%s\", \"%s\");"%(i, 'no title', 'test', 'test'))
+    print getStories(db, 5)
+    print getStories(db, 0)
+    print getStories(db, -2)
+    print getStories(db, 10)
+    
